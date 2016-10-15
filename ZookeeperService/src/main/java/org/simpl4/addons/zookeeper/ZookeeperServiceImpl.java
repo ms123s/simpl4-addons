02ac1ff2f9ee63e328f9b19bcd8bba64d2f71684
ms <manfred@ms123.org>
2016-10-15 06:46:08 +0200

    source was in wrong directory

--- ZookeeperService/src/main/java/org/simpl4/addons/zookeeper/ZookeeperServiceImpl.java
+++ ZookeeperService/src/main/java/org/simpl4/addons/zookeeper/ZookeeperServiceImpl.java
@@ -6,14 +6,18 @@ import org.apache.zookeeper.server.ServerConfig;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleActivator;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class ZookeeperServiceImpl extends ZooKeeperServerMain implements BundleActivator, ZookeeperService {
+	private static final Logger log = LoggerFactory.getLogger(ZookeeperService.class);
 
 	private Thread thread;
 	private ServerConfig config;
 
 	public void start(BundleContext context) {
 		System.out.println("ZookeeperService activate");
+		log.info("ZookeeperService activate");
 		config = new ServerConfig();
 		config.parse(new String[] { "2181", new File("data/zookeeper").getAbsolutePath() });
 		thread = new Thread(this::zk, "org.simpl4.addons.zookeeper");
@@ -28,10 +32,13 @@ public class ZookeeperServiceImpl extends ZooKeeperServerMain implements BundleA
 	public void zk() {
 		try {
 			System.out.println("ZookeeperService starting");
+			log.info("ZookeeperService starting");
 			runFromConfig(config);
 		} catch (Exception e) {
 			e.printStackTrace();
+			log.error("ZookeeperService:", e);
 		}
+		log.info("ZookeeperService exiting");
 		System.out.println("ZookeeperService exiting");
 	}
 }
