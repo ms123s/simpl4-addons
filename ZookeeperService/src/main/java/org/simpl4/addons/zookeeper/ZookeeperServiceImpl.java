8cbfc441bc5e6f341d6c4f77e39d0e62e5ce9b59
ms <manfred@ms123.org>
2016-10-16 22:30:57 +0200

    formatted

--- ZookeeperService/src/main/java/org/simpl4/addons/zookeeper/ZookeeperServiceImpl.java
+++ ZookeeperService/src/main/java/org/simpl4/addons/zookeeper/ZookeeperServiceImpl.java
@@ -2,15 +2,15 @@ package org.simpl4.addons.zookeeper;
 
 import java.io.File;
 
+import java.io.FileReader;
+import java.util.Properties;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
 import org.apache.zookeeper.server.ServerConfig;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleActivator;
-import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
-import org.slf4j.LoggerFactory;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
-import java.util.Properties;
-import java.io.FileReader;
+import org.slf4j.LoggerFactory;
 
 public class ZookeeperServiceImpl extends ZooKeeperServerMain implements BundleActivator, ZookeeperService {
 	private static final Logger log = LoggerFactory.getLogger(ZookeeperService.class);
@@ -21,7 +21,6 @@ public class ZookeeperServiceImpl extends ZooKeeperServerMain implements BundleA
 	public void start(BundleContext context) {
 		info("ZookeeperService activate");
 		config = getConfig();
-		//config.parse(new String[] { "2181", new File("data/zookeeper").getAbsolutePath() });
 		thread = new Thread(this::zk, "org.simpl4.addons.zookeeper");
 		thread.start();
 	}
@@ -30,29 +29,26 @@ public class ZookeeperServiceImpl extends ZooKeeperServerMain implements BundleA
 		shutdown();
 		thread.interrupt();
 	}
-	private ServerConfig getConfig(){
+
+	private ServerConfig getConfig() {
 		Properties properties = new Properties();
-		try{
-			properties.load( new FileReader( "etc/zookeeper.properties" ));
-		}catch(Exception e){
+		try {
+			properties.load(new FileReader("etc/zookeeper.properties"));
+		} catch (Exception e) {
 			throw new RuntimeException("ZookeeperServiceImpl.getProperties:", e);
 		}
 		QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
 		try {
 			quorumConfiguration.parseProperties(properties);
-		} catch(Exception e) {
-			throw new RuntimeException("ZookeeperServiceImpl.getConfig:",e);
+		} catch (Exception e) {
+			throw new RuntimeException("ZookeeperServiceImpl.getConfig:", e);
 		}
 		ServerConfig config = new ServerConfig();
 		config.readFrom(quorumConfiguration);
 		return config;
 	}
 
-	private void info(String msg){
-		System.out.println(msg);
-		log.info(msg);
-	}
-	public void zk() {
+	private void zk() {
 		try {
 			info("ZookeeperService starting");
 			runFromConfig(config);
@@ -61,6 +57,11 @@ public class ZookeeperServiceImpl extends ZooKeeperServerMain implements BundleA
 			log.error("ZookeeperService:", e);
 		}
 		info("ZookeeperService exiting");
+	}
+
+	private void info(String msg) {
+		System.out.println(msg);
+		log.info(msg);
 	}
 }
 
